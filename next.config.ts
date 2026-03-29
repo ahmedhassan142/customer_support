2f68c1cb4df991ab7d3ccb9c87b286d377625939
ahmed hassan <you@example.com>
2026-03-29 06:10:02 +0500

    second comit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  // TypeScript configuration
+  typescript: {
+    // Ignore TypeScript build errors during production builds
+    ignoreBuildErrors: true,
+  },
+  
+
 };
 
 export default nextConfig;
